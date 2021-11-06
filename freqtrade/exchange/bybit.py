e1e10009d2d00d26af7703a56b90f00bed5ea8fd
Rokas Kupstys <rokups@zoho.com>
2021-11-06 10:09:10 +0200

    Allow futures pairs on bybit.

--- freqtrade/exchange/bybit.py
+++ freqtrade/exchange/bybit.py
@@ -21,3 +21,12 @@ class Bybit(Exchange):
     _ft_has: Dict = {
         "ohlcv_candle_limit": 200,
     }
+
+    def market_is_tradable(self, market) -> bool:
+        """
+        Check if the market symbol is tradable by Freqtrade.
+        Default checks + check if pair is spot pair (no futures trading yet).
+        """
+        # BEGIN Futures/leverage
+        return market.get('future', False) or market.get('spot', False)
+        # END Futures/leverage
